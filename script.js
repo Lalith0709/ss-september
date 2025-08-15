f73b46fc2070490635c9088696cb71ec2e46f8ee
Lalith0709 <chlalithkrishnap@gmail.com>
2025-08-15 08:26:23 +0000

    mug2

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function showMessageCard() {
 function startConfetti() {
   setInterval(() => {
     confetti({
-      particleCount: 200,
+      particleCount: 250,
       spread: 100,
       origin: { y: 0 }
     });
@@ -62,13 +62,13 @@ function startFireworks() {
   let end = Date.now() + duration;
   (function frame() {
     confetti({
-      particleCount: 200,
+      particleCount: 250,
       angle: 60,
       spread: 100,
       origin: { x: 0 }
     });
     confetti({
-      particleCount: 200,
+      particleCount: 250,
       angle: 120,
       spread: 100,
       origin: { x: 1 }
