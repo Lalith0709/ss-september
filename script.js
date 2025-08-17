47f7554b5eeb041095942aad97aee98b0b271053
Lalith0709 <chlalithkrishnap@gmail.com>
2025-08-17 09:03:25 +0000

    kit24

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const birthdayDate = new Date("september 7, 2025 00:00:00").getTime();
+const birthdayDate = new Date("2025-09-07T00:00:00").getTime();
 
 function startCountdown(targetDate, callback) {
   let timer = setInterval(() => {
